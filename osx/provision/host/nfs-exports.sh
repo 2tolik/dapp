aa0ca5a18a92c45022597b678bae54ef8be9a856
Timofey Kirillov <timofey.kirillov@flant.com>
2018-04-23 17:35:14 +0300

    ++

--- osx/provision/host/nfs-exports.sh
+++ osx/provision/host/nfs-exports.sh
@@ -8,7 +8,6 @@ uid="$(id | grep -oP "uid=[0-9]+" | cut -d'=' -f2)"
 gid="$(id | grep -oP "gid=[0-9]+" | cut -d'=' -f2)"
 
 hostExports=(
-  "\"$HOME/.dapp/builds\" 172.28.128.3(rw,no_subtree_check,all_squash,anonuid=$uid,anongid=$gid)"
   "\"/tmp\" 172.28.128.3(rw,no_subtree_check,all_squash,anonuid=$uid,anongid=$gid)"
   "\"$GOPATH\" 172.28.128.3(rw,no_subtree_check,all_squash,anonuid=$uid,anongid=$gid)"
 )
